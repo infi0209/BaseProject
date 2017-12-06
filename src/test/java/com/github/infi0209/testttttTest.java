a9eb1c004444becb626dc05584468cd1f8220dd5
PC234 <infi0209@gmail.com>
2017-12-06 12:15:46 +0800

    testtttt

--- src/test/java/com/github/infi0209/testttttTest.java
+++ src/test/java/com/github/infi0209/testttttTest.java
@@ -1,15 +1,19 @@
-package com.github.infi0209;
+ package com.github.infi0209;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+    import org.junit.Assert;
+    import org.junit.Test;
 
 public class testttttTest {
     @Test
     public void getType() throws Exception {
+        // Arrange
+        Testtttt testtttt = new Testtttt(5, 10, 5);
 
-    T testtttt = new T
+        // Act
+        String result = testtttt.getType();
 
+        // Assert
+        Assert.assertEquals("不是三角形", result);
     }
 
 }
