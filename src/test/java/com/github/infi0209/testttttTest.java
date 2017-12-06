1b4c072588c93b92d653bffa942eb27483f91764
PC234 <infi0209@gmail.com>
2017-12-06 12:04:23 +0800

    testtttt

---
+++ src/test/java/com/github/infi0209/testttttTest.java
@@ -0,0 +1,15 @@
+package com.github.infi0209;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testttttTest {
+    @Test
+    public void getType() throws Exception {
+
+    T testtttt = new T
+
+    }
+
+}
